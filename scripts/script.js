26d100adf9cce3e2e67cc4c14c85d3f9f08a9830
Snowy Monkey <iagorafaelbg@gmail.com>
2022-08-04 15:20:44 -0300

    General layout n functions done v1.5

--- scripts/script.js
+++ scripts/script.js
@@ -24,13 +24,13 @@ function calc() {
   let qtTotalBottle = adults * bottlePP(duration) + kids * 0.5 * bottlePP(duration);
 
   // Display calculations
-//   The "+" symbol before the "=" means that the text wont be erased by the next line of text, without it, it would be showing only the last line
-result.innerHTML = `<h3 class="result-info">You will need:</h3>`
-result.innerHTML += `
+  //   The "+" symbol before the "=" means that the text wont be erased by the next line of text, without it, it would be showing only the last line
+  result.innerHTML = `<h3 class="result-info">You will need:</h3>`
+  result.innerHTML += `
   <div class="result-box">
   <p>
   <img src="./assets/meat.svg"/>
-  ${qtTotalMeat/1000} Kg of meat.
+  ${qtTotalMeat / 1000} Kg of meat.
   </p>
   <div>
   `
@@ -39,7 +39,7 @@ result.innerHTML += `
   <div class="result-box">  
   <p>
   <img src="./assets/can.svg"/>
-  ${Math.ceil(qtTotalBeer/0.355)} cans of beer (355ml).
+  ${Math.ceil(qtTotalBeer / 0.355)} beer cans (355ml).
   </p>
   <div>
   `
@@ -53,7 +53,7 @@ result.innerHTML += `
   <div>
   
   `
-  
+
   console.log(qtTotalMeat, " grams of meat.");
   console.log(qtTotalBeer, " L of beer.");
   console.log(qtTotalBottle, " L of soda/water.");
